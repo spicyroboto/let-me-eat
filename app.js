d7330a6f71dc0af312a621f2116bc338229bf9bc
seifghazi <seifghazi@gmail.com>
2019-03-07 00:06:58 -0800

    heroku deployment

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var body_parser = require('body-parser');
+const authRoutes = require('./backend/routes/auth_routes.js')
 const PORT = process.env.PORT || 3000;
 
 // --- INSTANTIATE THE APP
 var app = express();
+
+// Database initialization 
 
 app.use(express.static(__dirname + '/frontend'));
 
@@ -16,11 +19,20 @@ app.use(body_parser.json());
  app.engine('html', require('ejs').renderFile);
  app.set('frontend', 'html');
 
+ // ===== ROUTES ===== //
+ app.use(authRoutes);
+ // ------------------ //
+
 // --- ROUTING
 // Home page
+
 app.get('/', function(request, response) {
-    response.render('index.html');
+    response.send("LET ME EAT!!!!");
 });
+
+// app.get('/', function(request, response) {
+//     response.render('index.html');
+// });
 
 // --- START THE SERVER 
 var server = app.listen(PORT, function(){
