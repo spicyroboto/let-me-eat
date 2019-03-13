9d638983ec657e3ecb9d86ca88a0d422fe7323e4
eric <cgx1997@outlook.com>
2019-03-12 22:59:48 -0700

    also joins ingredient

--- sql_statements/queries.sql
+++ sql_statements/queries.sql
@@ -1,6 +1,10 @@
 SELECT distinct
-  food_item.name, restaurant.restaurantId, restaurant.name
+  food_item.name, ingredient.ingredientName, restaurant.restaurantId, restaurant.name
 FROM food_item
+INNER JOIN food_item_ingredients
+  ON food_item.foodItemId = food_item_ingredients.foodItemId
+INNER JOIN ingredient
+  ON food_item_ingredients.ingredientId = ingredient.ingredientId
 INNER JOIN part_of
   ON food_item.foodItemId = part_of.foodItemId
 INNER JOIN offered_items
