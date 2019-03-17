9f67b5ad2e47b2b35e2ad3374edb0b627761606f
Natasha Miner <nminer@live.ca>
2019-03-16 20:09:58 -0700

    Finished landing page for now

--- frontend/views/renderRestaurants.js
+++ frontend/views/renderRestaurants.js
@@ -34,25 +34,30 @@ function renderRestaurants(restaurant) {
 
     let html =  `<div class="card">
     <div class="card-header">
-    ${restaurant["name"]}
+    <b>${restaurant["name"]}</b>
     </div>
     <div class="card-body">
       <p class="card-text">
       <div class="row">
       <div class="col-md-4">
-      
+      <div class="restaurantHeader">Details</div>
       Cuisine Type: ${restaurant["cuisine"]}<br>
       Dining Type: ${restaurant["diningTypeName"]}<br>
       </div>
-      <div class="col-md-8">
+      <div class="col-md-4">
 
-      <b>Contact info</b><br>
-      <text class="phoneNo">${restaurant["phoneNo"]}</text><br>
-      ${restaurant["email"]}<br>
+      <div class="restaurantHeader">Address</div>
+      Location: ${restaurant["locationTag"]} <br>
       ${restaurant["streetName"]},
       ${restaurant["city"]},
       ${restaurant["province"]}<br>
+      ${restaurant["postalCode"]}
       </div>
+      <div class="col-md-4">
+      <div class="restaurantHeader">Contact Info</div>
+          <text class="phoneNo">${restaurant["phoneNo"]}</text><br>
+          ${restaurant["email"]}<br>
+          </div>
       </div>
 
 
